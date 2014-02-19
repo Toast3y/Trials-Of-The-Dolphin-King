900caf76909d619e174309a11c2ee188bf0a188d
Toast3y <pyrosburnttoast@gmail.com>
2014-02-19 17:20:17 +0000

    Added main menu

---
+++ Assets/MainMenuRoom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class MainMenuRoom : Room {
+
+
+
+	void OnEnter(){
+		Wait (1);
+		AddOption("New Game", MoveToNewGame);
+		AddOption("Exit", ExitGame);
+		Choose ("Choose your destiny!");
+	}
+	
+	void Onleave(){
+
+
+	}
+
+	void MoveToNewGame(){
+
+
+	}
+
+	void ExitGame(){
+		Application.Quit();
+	}
+}
