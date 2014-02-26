b86147db840ca35f972f40d86ec09dc9f992b937
Marcus Daly <daly.marcus@gmail.com>
2014-02-26 17:10:02 +0000

    Battle scripts edited and enemy created

--- Assets/BattleScreen.cs
+++ Assets/BattleScreen.cs
@@ -2,25 +2,32 @@
 using System.Collections;
 using Fungus;
 
-public class BattleScreen : Room {
+public class BattleScreen : Room 
+{
 
 	public Room Cockpit;
+    public Enemy fight = new Weakling();
 
 
-	void OnEnter(){
+	void OnEnter()
+    {
 		Wait (3);
 		Say ("This is fucked up");
+        //public Enemy fight = new Weakling();
+        Say("You encountered a " + fight.Name);
 		MoveToRoom (Cockpit);
 	}
 
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+
+    public class Enemy
+    {
+        public virtual string Name { get; set; }
+        public virtual int Health { get; set; }
+        public virtual int Engine { get; set; }
+        public virtual int Missles { get; set; }
+        public virtual int Lazerdam { get; set; }
+        public virtual int Acc { get; set; }
+
+    }
+
+    public class Weakling : Enemy //placeholdername
+    {
+        public override string Name { get { return "weakling"; } }
+        public override int Health { get { return 10; } }
+        public override int Engine { get { return 1; } }
+        public override int Missles { get { return 0; } }
+        public override int Lazerdam { get { return 5; } }
+        public override int Acc { get { return 75; } }
+
+    }
+
+    public class Stomper : Enemy //placeholdername
+    {
+        public override string Name { get { return "Stomper"; } }
+        public override int Health { get { return 15; } }
+        public override int Engine { get { return 2; } }
+        public override int Missles { get { return 0; } }
+        public override int Lazerdam { get { return 7; } }
+        public override int Acc { get { return 80; } }
+
+    }
+
+    public class SpaceOrca : Enemy //placeholdername
+    {
+        public override string Name { get { return "SpaceOrca"; } }
+        public override int Health { get { return 100; } }
+        public override int Engine { get { return 5; } }
+        public override int Missles { get { return 0; } }
+        public override int Lazerdam { get { return 10; } }
+        public override int Acc { get { return 60; } }
+
+    }
